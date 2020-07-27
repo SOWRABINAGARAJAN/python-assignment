1de794cf70af6a7c8c0ecf188ef00a0410590d3f
Sowrabi Nagarajan <53907950+SOWRABINAGARAJAN@users.noreply.github.com>
2020-07-27 21:59:03 +0530

    Day 6 Assignment uploaded

---
+++ Day 6/1.py
@@ -0,0 +1,9 @@
+#Convert to a dictionary in one line code using list comprehension (without using zip method)
+
+List1 = [1,2,3,4,5,6,7,8]
+
+List2 = ["a","b","c","d","e"]
+
+new_dict = { List1[i]:List2[i] for i in range(len(List2))}
+
+print(new_dict)
